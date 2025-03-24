47ccdde7eadcf0d521358f3c775704ce95c08243
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2025-03-24 21:54:13 +0530

    prisma adapter added with next-auth

--- apps/frontend/auth.ts
+++ apps/frontend/auth.ts
@@ -1,13 +1,11 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
-
-console.log("google client id: ", process.env.GOOGLE_CLIENT_ID,
-    "google client secret: ", process.env.GOOGLE_CLIENT_SECRET,
-    "github client id: ", process.env.GITHUB_CLIENT_ID,
-    "github client secret: ", process.env.GITHUB_CLIENT_SECRET,)
+import { prisma } from "./prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: PrismaAdapter(prisma),
     providers: [Google({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
